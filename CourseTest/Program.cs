636e0680b99190a351333372facd229ca29e9ef9
artur <artur@GEORGE>
2023-12-19 21:58:16 +0300

    SubResult

--- CourseTest/Program.cs
+++ CourseTest/Program.cs
@@ -1,18 +1,18 @@
 ﻿using Microsoft.Data.SqlClient;
-using System.Data;
 using System.Diagnostics;
-
-
 
 class PingPong
 {
     static string tabel = "Sales.OrderTracking";
-    static int Menu() 
+    static string tabelPasswords = "Person.Password";
+    static int Menu()
     {
         int answer;
 
         Console.WriteLine("Choose what action do you want to do:");
         Console.WriteLine("1. Find order ...");
+        Console.WriteLine("2. Get hash of table");
+        Console.WriteLine("3. Sort id of orders");
 
         answer = Convert.ToInt32(Console.ReadLine());
 
@@ -29,24 +29,29 @@ class PingPong
                 int operation = Menu();
                 SqlConnection cnn = SqlConnect();
 
-                switch (operation) 
+                switch (operation)
                 {
                     case 1:
                         TopSearchItem(cnn, comm);
+                        SequentalSearch("790 Shelbyville Road`");
                         break;
-                    default:
-
+                    case 2:
+                        TopHash(cnn, comm);
+                        SequentalHash();
+                        break;
+                    case 3:
+                        TopSorting(cnn, comm);
+                        SequentalSorting();
                         break;
                 }
 
-              
+
 
                 for (int dest = 1; dest < comm.Size; ++dest)
                 {
                     comm.Send("MPI.Kill", dest, 0);
                 }
 
-                SequentalSearch("qwe");
 
             }
             else
@@ -59,7 +64,6 @@ class PingPong
 
     static void Parallel(MPI.Intracommunicator comm)
     {
-        Console.WriteLine("Waiting command...");
         int command = comm.Receive<int>(0, 0);
 
         switch (command)
@@ -67,25 +71,34 @@ class PingPong
             case 1:
                 SubSearchForItem(comm);
                 break;
+            case 2:
+                SubHash(comm);
+                break;
+            case 3:
+                SubSorting(comm);
+                break;
         }
 
     }
 
-    static void TopSearchItem(SqlConnection cnn, MPI.Intracommunicator comm) 
+    static void TopSearchItem(SqlConnection cnn, MPI.Intracommunicator comm)
     {
         List<string> subElements;
         string searchTarget = "790 Shelbyville Road";
         string query = $"SELECT CarrierTrackingNumber from {tabel}";
+        Stopwatch stopWatch = new Stopwatch();
 
 
-        for (int dest = 1; dest < comm.Size; ++dest) 
+        for (int dest = 1; dest < comm.Size; ++dest)
         {
             comm.Send(1, dest, 0);
         }
-        Console.WriteLine("sent packets");
+
 
         List<string> elements = MakeRequest(cnn, query);
         cnn.Close();
+
+        stopWatch.Start();
 
         int mpiSize = comm.Size - 1;
         int portionSize = elements.Count / mpiSize;
@@ -107,10 +120,18 @@ class PingPong
 
         }
 
+        stopWatch.Stop();
+
+        printTime(stopWatch, "Sending data ended with time: ");
+
+        stopWatch.Start();
+
         for (int i = 1; i < comm.Size; i++)
         {
             Console.WriteLine(comm.Receive<string>(i, 1));
         }
+
+        printTime(stopWatch, "Mission completed with time: ");
     }
 
 
@@ -122,7 +143,7 @@ class PingPong
             string searchTarget;
             string answer = "Not found";
 
-            Console.WriteLine("Waiting search target...");
+            //Console.WriteLine("Waiting search target...");
             searchTarget = comm.Receive<string>(0, 0);
 
             Stopwatch stopWatch = new Stopwatch();
@@ -146,12 +167,164 @@ class PingPong
             comm.Send(answer, 0, 1);
 
             stopWatch.Stop();
-            TimeSpan ts = stopWatch.Elapsed;
-            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-            ts.Hours, ts.Minutes, ts.Seconds,
-            ts.Milliseconds / 10);
 
-            Console.WriteLine("Task was completed within: {0}", elapsedTime);
+            printTime(stopWatch, "Subtask ended with time: ");
+
+            //Console.WriteLine("{0} process completed task! with size {1}", MPI.Environment.HostRank, part.Count);
+            //zConsole.WriteLine("Last element: {0}", part.Last());
+        }
+
+
+    }
+    static void TopHash(SqlConnection cnn, MPI.Intracommunicator comm)
+    {
+        List<string> subElements;
+        List<string> result = new List<string>(2048);
+        string searchTarget = "null";
+        string query = $"SELECT PasswordHash from {tabelPasswords}";
+
+
+        for (int dest = 1; dest < comm.Size; ++dest)
+        {
+            comm.Send(2, dest, 0);
+        }
+
+        List<string> elements = MakeRequest(cnn, query);
+        cnn.Close();
+
+
+        Stopwatch stopWatch = new Stopwatch();
+        stopWatch.Start();
+
+        int mpiSize = comm.Size - 1;
+        int portionSize = elements.Count / mpiSize;
+
+        for (int dest = 1; dest < comm.Size; ++dest)
+        {
+            comm.Send(searchTarget, dest, 0);
+
+            if (dest != (comm.Size - 1))
+            {
+                subElements = elements.GetRange((dest - 1) * portionSize, portionSize);
+                comm.Send(subElements, dest, 0);
+            }
+            else
+            {
+                subElements = elements.GetRange((dest - 1) * portionSize, elements.Count - ((dest - 1) * portionSize));
+                comm.Send(subElements, dest, 0);
+            }
+
+        }
+
+        stopWatch.Stop();
+
+        printTime(stopWatch, "Sending data ended with time: ");
+
+        stopWatch.Start();
+
+        for (int i = 1; i < comm.Size; i++)
+        {
+            result.AddRange(comm.Receive<List<string>>(i, 1));
+        }
+
+        stopWatch.Stop();
+        printTime(stopWatch, "Mission completed with time: ");
+    }
+
+    static void TopSorting(SqlConnection cnn, MPI.Intracommunicator comm)
+    {
+        List<int> subElements;
+        List<int> result = new List<int>(2048);
+        string searchTarget = "null";
+        string query = $"SELECT SalesOrderID from {tabel}";
+
+
+        for (int dest = 1; dest < comm.Size; ++dest)
+        {
+            comm.Send(3, dest, 0);
+        }
+
+        List<int> elements = MakeRequestInt(cnn, query);
+        cnn.Close();
+
+
+        Stopwatch stopWatch = new Stopwatch();
+        stopWatch.Start();
+
+        int mpiSize = comm.Size - 1;
+        int portionSize = elements.Count / mpiSize;
+
+        for (int dest = 1; dest < comm.Size; ++dest)
+        {
+            comm.Send(searchTarget, dest, 0);
+
+            if (dest != (comm.Size - 1))
+            {
+                subElements = elements.GetRange((dest - 1) * portionSize, portionSize);
+                comm.Send(subElements, dest, 0);
+            }
+            else
+            {
+                subElements = elements.GetRange((dest - 1) * portionSize, elements.Count - ((dest - 1) * portionSize));
+                comm.Send(subElements, dest, 0);
+            }
+
+        }
+
+        stopWatch.Stop();
+
+        printTime(stopWatch, "Sending data ended with time: ");
+
+        stopWatch.Start();
+
+        for (int i = 1; i < comm.Size; i++)
+        {
+            result.AddRange(comm.Receive<List<int>>(i, 1));
+        }
+
+        result.Sort();
+
+        //for (int i = 1; i < 10; i++){
+        //    Console.WriteLine(result[i]);
+        //}
+
+        stopWatch.Stop();
+        printTime(stopWatch, "Mission completed with time: ");
+    }
+
+
+    static void SubHash(MPI.Intracommunicator comm)
+    {
+        while (true)
+        {
+            List<string> part;
+            string searchTarget;
+            List<string> answer = new List<string>(100);
+
+            searchTarget = comm.Receive<string>(0, 0);
+
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            if (searchTarget == "MPI.Kill")
+            {
+                break;
+            }
+
+            part = comm.Receive<List<string>>(0, 0);
+
+            foreach (string item in part)
+            {
+
+                answer.Add(Convert.ToString(item.GetHashCode()));
+
+            }
+
+            comm.Send(answer, 0, 1);
+
+            stopWatch.Stop();
+
+            printTime(stopWatch, "Subtask ended with time: ");
 
             //Console.WriteLine("{0} process completed task! with size {1}", MPI.Environment.HostRank, part.Count);
             //zConsole.WriteLine("Last element: {0}", part.Last());
@@ -160,6 +333,37 @@ class PingPong
 
     }
 
+    static void SubSorting(MPI.Intracommunicator comm)
+    {
+        while (true)
+        {
+            List<int> part;
+            string searchTarget;
+            List<string> answer = new List<string>(100);
+
+            searchTarget = comm.Receive<string>(0, 0);
+
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            if (searchTarget == "MPI.Kill")
+            {
+                break;
+            }
+
+            part = comm.Receive<List<int>>(0, 0);
+
+            part.Sort();
+
+            comm.Send(part, 0, 1);
+
+            stopWatch.Stop();
+
+            printTime(stopWatch, "Subtask ended with time: ");
+        }
+
+
+    }
 
     static void SequentalSearch(string searchTarget)
     {
@@ -185,12 +389,53 @@ class PingPong
 
 
         stopWatch.Stop();
-        TimeSpan ts = stopWatch.Elapsed;
-        string elapsedTime = String.Format("Sequental: {0:00}:{1:00}:{2:00}.{3:00}",
-        ts.Hours, ts.Minutes, ts.Seconds,
-        ts.Milliseconds / 10);
 
-        Console.WriteLine(elapsedTime);
+        printTime(stopWatch, "Sequental ended with time: ");
+
+    }
+
+    static void SequentalHash()
+    {
+        Stopwatch stopWatch = new Stopwatch();
+        stopWatch.Start();
+
+        string query = $"SELECT PasswordHash from {tabelPasswords}";
+
+        SqlConnection cnn = SqlConnect();
+        List<string> elements = MakeRequest(cnn, query);
+        cnn.Close();
+
+        List<string> answer = new List<string>(100);
+
+        foreach (string item in elements)
+        {
+
+            answer.Add(Convert.ToString(item.GetHashCode()));
+        }
+
+        stopWatch.Stop();
+
+        printTime(stopWatch, "Sequental ended with time: ");
+
+    }
+
+    static void SequentalSorting()
+    {
+        Stopwatch stopWatch = new Stopwatch();
+        stopWatch.Start();
+
+        string query = $"SELECT SalesOrderID from {tabel}";
+
+        SqlConnection cnn = SqlConnect();
+        List<int> elements = MakeRequestInt(cnn, query);
+        cnn.Close();
+
+        elements.Sort();
+        //BubbleSort(ref elements);
+
+        stopWatch.Stop();
+
+        printTime(stopWatch, "Sequental ended with time: ");
 
     }
 
@@ -217,5 +462,48 @@ class PingPong
         }
 
         return elements;
+    }
+
+    static List<int> MakeRequestInt(SqlConnection cnn, string query)
+    {
+        SqlCommand command = new SqlCommand(query, cnn);
+        SqlDataReader reader = command.ExecuteReader();
+
+        List<int> elements = new List<int>(2048);
+        while (reader.Read())
+        {
+            elements.Add((int)reader.GetValue(0));
+        }
+
+        return elements;
+    }
+
+    static void printTime(Stopwatch watch, String description)
+    {
+        TimeSpan ts = watch.Elapsed;
+        Console.WriteLine(description + String.Format("Sequental: {0:00}:{1:00}:{2:00}.{3:00}",
+        ts.Hours, ts.Minutes, ts.Seconds,
+        ts.Milliseconds / 10));
+    }
+
+    static void BubbleSort(ref List<int> a)
+    {
+        int temp;
+
+        foreach (int aa in a)
+
+            for (int p = 0; p <= a.Count - 2; p++)
+            {
+                for (int i = 0; i <= a.Count - 2; i++)
+                {
+                    if (a[i] > a[i + 1])
+                    {
+                        temp = a[i + 1];
+                        a[i + 1] = a[i];
+                        a[i] = temp;
+                    }
+                }
+            }
+
     }
 }
